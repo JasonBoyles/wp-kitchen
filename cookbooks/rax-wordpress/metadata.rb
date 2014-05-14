99026cb6541168d76db4b70dc368369de36b195f
Jason Boyles <jason@duncancreek.net>
2014-05-14 12:08:58 -0500

    first commit

---
+++ cookbooks/rax-wordpress/metadata.rb
@@ -0,0 +1,12 @@
+name    'rax-wordpress'
+version '0.1.0'
+license 'Apache 2.0'
+
+%w{ rax-mysql-tunables apache database firewall iptables-ng lsyncd mysql
+    varnish wordpress memcached }.each do |cb|
+  depends cb
+end
+
+%w{ ubuntu debian centos rhel }.each do |os|
+  supports os
+end
